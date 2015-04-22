17717c0d8eb0377815fec59a3152cf4a8edaf8b5
Eric Ianni <iannie@onid.oregonstate.edu>
2015-04-22 14:35:45 -0400

    Ianni - I fixed the Algorithm1 to better reflect the assignment

--- algorithms.cpp
+++ algorithms.cpp
@@ -7,47 +7,73 @@ Input an array, output the subarray with a maximum sum along with the sum
 
 //Algorithm 1: Enumeration
 //need improved,this is better enumeration
+//int algorithm_1(vector<int> nums, vector<int> &subarray){
+//	int sum, max_sum = 0; 
+//	int start_idx, end_idx;
+//	for(int i = 0; i < nums.size(); i++){
+//		sum = 0;
+//		for(int j = i; j < nums.size(); j++){
+//			sum += nums[j]; 
+//			if(sum > max_sum){
+//				max_sum = sum;
+//				start_idx = i;
+//				end_idx = j;
+//			}
+//		}
+//	}
+//	subarray.clear();
+//	for(int j = start_idx; j <= end_idx; j++){
+//		subarray.push_back(nums[j]);
+//	}
+//	return max_sum;
+//}
+//
+
+//Algorithm 1: Enumeration
+//Ianni's attempt
 int algorithm_1(vector<int> nums, vector<int> &subarray){
-	int sum, max_sum = 0; 
-	int start_idx, end_idx;
-	for(int i = 0; i < nums.size(); i++){
-		sum = 0;
-		for(int j = i; j < nums.size(); j++){
-			sum += nums[j]; 
-			if(sum > max_sum){
-				max_sum = sum;
-				start_idx = i;
-				end_idx = j;
-			}
-		}
-	}
-	subarray.clear();
-	for(int j = start_idx; j <= end_idx; j++){
-		subarray.push_back(nums[j]);
-	}
-	return max_sum;
+  int sum, max_sum = 0; 
+  int start_idx, end_idx;
+  for(int i = 0; i < nums.size(); i++){
+    for(int j = i; j < nums.size(); j++){
+      sum = 0;
+      for(int a = i a <= j; a++) {
+	sum += nums[j];
+      }
+      if(sum > max_sum){
+	max_sum = sum;
+	start_idx = i;
+	end_idx = j;
+      }
+    }
+  }
+  subarray.clear();
+  for(int j = start_idx; j <= end_idx; j++){
+    subarray.push_back(nums[j]);
+  }
+  return max_sum;
 }
 
 //Algorithm 2: Better Enumeration
 int algorithm_2(vector<int> nums, vector<int> &subarray){
-        int sum, max_sum = 0;    
-        int start_idx, end_idx;
-        for(int i = 0; i < nums.size(); i++){
-                sum = 0;
-                for(int j = i; j < nums.size(); j++){
-                        sum += nums[j];
-                        if(sum > max_sum){
-                                max_sum = sum;
-                                start_idx = i;
-                                end_idx = j;
-                        }
-                }
-        }
-        subarray.clear();
-        for(int j = start_idx; j <= end_idx; j++){
-                subarray.push_back(nums[j]);
-        }
-        return max_sum;
+  int sum, max_sum = 0;    
+  int start_idx, end_idx;
+  for(int i = 0; i < nums.size(); i++){
+    sum = 0;
+    for(int j = i; j < nums.size(); j++){
+      sum += nums[j];
+      if(sum > max_sum){
+	max_sum = sum;
+	start_idx = i;
+	end_idx = j;
+      }
+    }
+  }
+  subarray.clear();
+  for(int j = start_idx; j <= end_idx; j++){
+    subarray.push_back(nums[j]);
+  }
+  return max_sum;
 }
 
 //Algorithm 3: Divide and conquer
